2acac9fd3aa2e23992fd82a9f868d300c502f13d
Aditya Singh <adityaa9170843@gmail.com>
2025-07-18 12:44:55 +0530

    refactor: clean up authController.js for improved readability and consistency

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,59 +1,78 @@
-
-import {UserModel} from '../models/User.js';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-
+import { UserModel } from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const generateToken = (user) => {
-    return jwt.sign({email: user.email, id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    })
-}
+  return jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
-export const registerUser = async(req, res) => {
-    try{
-        const {name,email,password} = req.body;
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        const User = await UserModel.find({email});
-        if(User){
-            res.status(400);
-            throw new Error("User already exists");
+    const ExistedUser = await UserModel.findOne({ email });
+    if (ExistedUser) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          res.status(400);
+          throw new Error("Error hashing password");
         }
+        const user = await UserModel.create({
+          name,
+          email,
+          password: hash,
+        });
+        let token = generateToken(user);
+        res.cookie("token", token);
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token,
+        });
+        console.log(token);
+      });
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(password,salt, async(err, hash) => {
-                if(err){
-                    res.status(400);
-                    throw new Error("Error hashing password");
-                }
-                const user = await UserModel.create({
-                    name,
-                    email,
-                    password: hash
-                })
-                let token = generateToken(user);
-                res.cookie("token",token)
-                res.status(201).json({
-                    _id: user._id,
-                    name: user.name,
-                    email: user.email,
-                    token
-                })
-                console.log(token);
-                
-            })
-        })
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = UserModel.findOne({ email });
+    if (!user) {
+      res.status(400);
+      throw new Error("User does not exist");
     }
-    catch(error){
-        res.status(400);
-        throw new Error(error.message);
-    }
-}
 
-export const loginUser = async(req, res) => {
-    
-}
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) return res.send("password not matched");
+      else if (result) {
+        let token = generateToken(user);
+        res.cookie("token", token);
+        res.status(200).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token,
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+};
 
-export const getUserProfile = async(req, res) => {
-    
-}
+export const getUserProfile = async (req, res) => {};

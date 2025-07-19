0479ff624ee97e6475345d0fe7f35c5c4cbd3ac9
Aditya Singh <adityaa9170843@gmail.com>
2025-07-19 22:01:38 +0530

    refactor: rename getSessions to getMySessions and update session route imports

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,5 +1,5 @@
 import { SessionModel } from "../models/Session.js";
-import { QuestionModel } from "../models/Question.js";
+import { QuestionModel } from "../models/Question.js ";
 
 export const createSession = async (req, res) => {
   try {
@@ -17,7 +17,6 @@ export const createSession = async (req, res) => {
 
     const questionIds = [];
     for (const qus of questions) {
-      
       const questionDoc = await QuestionModel.create({
         session: session._id,
         question: qus.question,
@@ -25,13 +24,10 @@ export const createSession = async (req, res) => {
       });
       questionIds.push(questionDoc._id);
     }
-   
 
     session.questions = questionIds;
     await session.save();
     res.status(201).json(session);
-
-
   } catch (error) {
     res.status(400);
     console.log("got error", error);
@@ -39,8 +35,16 @@ export const createSession = async (req, res) => {
   }
 };
 
-export const getSessions = async (req, res) => {
+export const getMySessions = async (req, res) => {
   try {
+    const userId = req.LoggedInUser._id;
+    const session = await SessionModel.find({ user: userId })
+      .sort({
+        createdAt: -1,
+      })
+      .populate("questions");
+
+    res.status(200).json(session);
   } catch (error) {
     res.status(400);
     console.log("got error", error);
@@ -50,6 +54,15 @@ export const getSessions = async (req, res) => {
 
 export const getSessionById = async (req, res) => {
   try {
+    const sessionId = req.params.id;
+    const session = await SessionModel.findById(sessionId)
+      .populate({ path: "questions", options: { sort: { createdAt: -1 } } })
+      
+      if(!session){
+        res.status(400);
+        throw new Error("Session does not exist");
+      }
+      res.status(200).json(session);
   } catch (error) {
     res.status(400);
     console.log("got error", error);
@@ -59,6 +72,15 @@ export const getSessionById = async (req, res) => {
 
 export const deleteSession = async (req, res) => {
   try {
+    const sessionId = req.params.id;
+   const session = await SessionModel.findById(sessionId);
+   if(!session){
+    res.status(400);
+    throw new Error("Session does not exist");
+   }
+    await QuestionModel.deleteMany({ session: sessionId });
+    await SessionModel.findByIdAndDelete(sessionId);
+    res.status(200).json({ message: "Session deleted successfully" });
   } catch (error) {
     res.status(400);
     console.log("got error", error);
--- backend/routes/sessionRoute.js
+++ backend/routes/sessionRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { createSession, deleteSession, getSessionById, getSessions } from "../controllers/sessionController.js";
+import { createSession, deleteSession, getSessionById, getMySessions } from "../controllers/sessionController.js";
 
 const router = express.Router();
 
 router.post('/create',protect,createSession)
-router.get('/my-sessions',protect,getSessions)
+router.get('/my-sessions',protect,getMySessions)
 router.get("/:id",protect,getSessionById)
 router.delete("/:id",protect,deleteSession)
 
